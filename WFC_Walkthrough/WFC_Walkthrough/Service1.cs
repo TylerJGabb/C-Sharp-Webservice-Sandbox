dca5b560149a8723a738b4a1b5b212589e199650
Tyler Gabb <developer@polyprint.com>
2018-03-16 10:49:44 -0700

    first commit

---
+++ WFC_Walkthrough/WFC_Walkthrough/Service1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WFC_Walkthrough
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+
+
+        public string GetData(string value)
+        {
+            return value + " I got your data with the value";
+        }
+
+        public void MutateCompositeType(ref CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+        }
+    }
+}
